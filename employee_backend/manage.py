0e79fc90643501bc3fec474b39c325309fe06b27
Venkatesh <40453474+sapsecops@users.noreply.github.com>
2025-08-14 15:05:05 +0530

    Update manage.py

--- employee_backend/manage.py
+++ employee_backend/manage.py
@@ -3,7 +3,7 @@ import os
 import sys
 
 def main():
-    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "backend.settings")
+    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "employee_backend.settings")
     from django.core.management import execute_from_command_line
     execute_from_command_line(sys.argv)
 
