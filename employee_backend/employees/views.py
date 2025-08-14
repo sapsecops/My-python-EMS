fe4341cffad1254991cdf4b78bba78c6a35c3522
techizone <venkateshsap413@gmail.com>
2025-08-14 12:01:22 +0530

    Deleted all Project

--- employee_backend/employees/views.py
+++
@@ -1,8 +0,0 @@
-from rest_framework import viewsets
-from .models import Employee
-from .serializers import EmployeeSerializer
-from rest_framework.response import Response
-
-class EmployeeViewSet(viewsets.ModelViewSet):
-    queryset = Employee.objects.all()
-    serializer_class = EmployeeSerializer
